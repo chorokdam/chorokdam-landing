a8a750759fa992b76f9cf5c6ec421ebd436b88af
chorokdam <happy.heestory@gmail.com>
2025-11-23 16:44:00 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import "./index.css"; // ⭐ 반드시 가장 위에 있어야 Tailwind 적용됨
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
